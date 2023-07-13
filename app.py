839f82bd20ca2c9835c559f8f75dfbe4345b2eee
kys <kys119@sen.go>
2023-07-13 19:38:13 +0900

    ADD:caching

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ import requests
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from utils.img_preprocessing import catch_face, is_image
+from utils.help_caching import make_cache_key
 import os
 import cv2
 import tensorflow as tf
 import numpy as np
 from tensorflow.keras.models import load_model
+from flask_caching import Cache
 
 app = Flask(__name__)
-
+app.config.from_mapping({"DEBUG": True, "CACHE_TYPE": "simple", "CACHE_DEFAULT_TIMEOUT": 300})
 test_model = load_model('models/model_siamese_neural_network.h5')
+cache = Cache(app)
 
 load_dotenv()
 
@@ -18,11 +21,13 @@ ESNTLID = os.getenv("ESNTLID")
 AUTHKEY = os.getenv("AUTHKEY")
 
 @app.route('/api/find', methods=['GET'])
+@cache.cached(timeout=3600, key_prefix=make_cache_key)
 def search_missing_person():
-    name = request.args.get('name', default=None, type=str)
+    name = request.args.get('name', default=" ", type=str)
     age = request.args.get('age', default=None, type=int)
+
     if name is not None or age is not None :
-        res = requests.post("https://www.safe182.go.kr/api/lcm/findChildList.do", params={"esntlId" : ESNTLID, "authKey" : AUTHKEY, "rowSize" : "100", "nm" : name, "age" : age}) 
+        res = requests.post("https://www.safe182.go.kr/api/lcm/findChildList.do", params={"esntlId" : ESNTLID, "authKey" : AUTHKEY, "rowSize" : "100", "nm" : name, "age1" : age}) 
         return res.json()
     else:
         return jsonify({"error" : "no input value"})
@@ -54,7 +59,6 @@ def use_ai():
         result = round(test_model.predict([image, image2])[0][0] * 100)
 
         return jsonify({"ai_result" : result})
-    
     except:
         return jsonify({"error" : "error"})
      
